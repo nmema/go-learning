acdaa445367ae035947074ddd190a4bbee044bb8
nmema <nicolas.a.mema@gmail.com>
2023-02-25 21:09:35 -0300

    #11 add improve speed using goroutines

--- cli-go/chapter-05/colStats/main.go
+++ cli-go/chapter-05/colStats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -42,28 +43,55 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 
 	consolidate := make([]float64, 0)
 
+	// Create the channel to receive results of errors of operations
+	resCh := make(chan []float64)
+	errCh := make(chan error)
+	doneCh := make(chan struct{})
+
+	wg := sync.WaitGroup{}
+
 	// Loop through all files adding their data to consolidate
 	for _, fname := range filenames {
-		// Open the file for reading
-		f, err := os.Open(fname)
-		if err != nil {
-			return fmt.Errorf("Cannot open file: %w", err)
-		}
+		wg.Add(1)
 
-		// Parse the CSV into a slice of float64 numbers
-		data, err := csv2float(f, column)
-		if err != nil {
-			return err
-		}
+		go func(fname string) {
+			defer wg.Done()
 
-		if err := f.Close(); err != nil {
-			return err
-		}
+			// Open the file for reading
+			f, err := os.Open(fname)
+			if err != nil {
+				errCh <- fmt.Errorf("Cannot open file: %w", err)
+				return
+			}
 
-		// Append the data to consolidate
-		consolidate = append(consolidate, data...)
+			// Parse the CSV into a slice of float64 numbers
+			data, err := csv2float(f, column)
+			if err != nil {
+				errCh <- err
+			}
+
+			if err := f.Close(); err != nil {
+				errCh <- err
+			}
+
+			resCh <- data
+		}(fname)
 	}
 
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case data := <-resCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
+			return err
+		}
+	}
 }
